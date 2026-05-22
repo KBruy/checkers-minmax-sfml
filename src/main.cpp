1c879d9d6c39ca6bbbd041a49bc4d4c9e9df2667
KBruy <krystian.bury1@gmail.com>
2026-05-22 14:50:52 +0200

    feat: display ai move with delay

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,34 @@
 #include "Board.h"
 #include "Move.h"
 
+#include <chrono>
 #include <iostream>
 #include <string>
+#include <thread>
 #include <vector>
+
+std::string positionToText(int row, int col) {
+    char file = 'a' + col;
+    char rank = '8' - row;
+
+    std::string position = "";
+    position += file;
+    position += rank;
+
+    return position;
+}
+
+void printMove(const Move& move) {
+    for (int i = 0; i < static_cast<int>(move.rows.size()); i++) {
+        std::cout << positionToText(move.rows[i], move.cols[i]);
+
+        if (i < static_cast<int>(move.rows.size()) - 1) {
+            std::cout << " -> ";
+        }
+    }
+
+    std::cout << "\n";
+}
 
 int main() {
     Board board;
@@ -48,8 +73,30 @@ int main() {
             break;
         }
 
+        int captureCount = 0;
+        int longestCaptureSteps = 0;
+
+        for (int i = 0; i < static_cast<int>(legalMoves.size()); i++) {
+            if (legalMoves[i].isCapture) {
+                captureCount++;
+
+                int steps = static_cast<int>(legalMoves[i].rows.size()) - 1;
+
+                if (steps > longestCaptureSteps) {
+                    longestCaptureSteps = steps;
+                }
+            }
+        }
+
         std::cout << "-------------------------------\n";
         std::cout << "Liczba legalnych ruchow: " << legalMoves.size() << '\n';
+        std::cout << "Liczba bic wsrod legalnych ruchow: " << captureCount << '\n';
+
+        if (captureCount > 0) {
+            std::cout << "Najdluzsza sekwencja bicia: "
+                      << longestCaptureSteps << " ruch/y\n";
+        }
+
         std::cout << "Ocena dla bialych: " << board.evaluate('w') << "\n";
         std::cout << "Ocena dla czarnych: " << board.evaluate('b') << "\n";
         std::cout << "-------------------------------\n\n";
@@ -57,12 +104,19 @@ int main() {
         if (currentPlayer == 'b') {
             std::cout << "Tura komputera, czarne mysla...\n";
 
+            std::this_thread::sleep_for(std::chrono::milliseconds(700));
+
             Move aiMove = ai.findBestMove(board);
 
             if (aiMove.rows.size() < 2) {
                 std::cout << "Komputer nie znalazl ruchu.\n";
                 break;
             }
+
+            std::cout << "Komputer wybral ruch: ";
+            printMove(aiMove);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(700));
 
             board.applyMove(aiMove);
 
@@ -108,14 +162,14 @@ int main() {
 
         if (forcedFrom != "" && !captureMove) {
             std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
-            std::cout << "      Musisz kontynuowac bicie.\n";
+            std::cout << "Musisz kontynuowac bicie.\n";
             std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n";
             continue;
         }
 
         if (mustCapture && !captureMove) {
             std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
-            std::cout << "      Musisz wykonac bicie.\n";
+            std::cout << "Musisz wykonac bicie.\n";
             std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n";
             continue;
         }
@@ -135,7 +189,7 @@ int main() {
             }
         } else {
             std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n";
-            std::cout << "        Niepoprawny ruch.\n";
+            std::cout << "         Niepoprawny ruch.\n";
             std::cout << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n";
         }
     }
